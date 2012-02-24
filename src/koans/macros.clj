02dab2ea49446237679480893a0c15031c9cf618
Isaac Sanders <isaacbfsanders@gmail.com>
2012-02-24 16:26:18 -0500

    I have achieved clojure enlightenment

--- src/koans/macros.clj
+++ src/koans/macros.clj
@@ -10,14 +10,15 @@
     ~(nth form 2)))
 
 (defmacro r-infix [form]
+  (print form "\n")
   (cond (not (seq? form))
-          __
+          form
         (= 1 (count form))
           `(r-infix ~(first form))
         :else
           (let [operator (second form)
                 first-arg (first form)
-                others __]
+                others (rest (rest form))]
             `(~operator
               (r-infix ~first-arg)
               (r-infix ~others)))))
@@ -40,4 +41,5 @@
   (= (quote (+ 10 (2 * 3))) (macroexpand '(infix-better ( 10 + (2 * 3)))))
 
   "Really, you dont understand recursion until you understand recursion"
+  (= 36 (r-infix (10 + (2 * 3) + (4 * 5)))))
 
